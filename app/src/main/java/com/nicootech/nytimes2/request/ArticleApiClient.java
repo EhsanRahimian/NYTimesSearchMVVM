a222e19564adc696e5ba97fec4ea71dc38207aa0
Ethan Rahimian <nicootech@gmail.com>
2021-08-17 09:13:18 -0700

    6-Executing a Runnable with an Executor

--- app/src/main/java/com/nicootech/nytimes2/request/ArticleApiClient.java
+++ app/src/main/java/com/nicootech/nytimes2/request/ArticleApiClient.java
@@ -1,20 +1,32 @@
 package com.nicootech.nytimes2.request;
 
+import android.util.Log;
+
 import com.nicootech.nytimes2.AppExecutors;
 import com.nicootech.nytimes2.models.Docs;
+import com.nicootech.nytimes2.request.responses.ArticleSearchResponse;
+import com.nicootech.nytimes2.util.Constants;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
+import retrofit2.Call;
+import retrofit2.Response;
 
 import static com.nicootech.nytimes2.util.Constants.NETWORK_TIMEOUT;
 
 public class ArticleApiClient {
 
+    private static final String TAG = "ArticleApiClient";
+
     private static ArticleApiClient instance;
     private MutableLiveData<List<Docs>> mDocs;
+    private RetrieveArticleRunnable mRetrieveArticleRunnable;
 
     public static ArticleApiClient getInstance() {
         if (instance == null) {
@@ -31,14 +43,12 @@ public class ArticleApiClient {
         return mDocs;
     }
 
-    public void searchArticlesApi(){
-        final Future handler = AppExecutors.getInstance().networkIO().submit(new Runnable() {
-            @Override
-            public void run() {
-                //retrieve data from rest api
-                // mDocs.postValue();
-            }
-        });
+    public void searchArticlesApi(String query, int pageNumber){
+        if(mRetrieveArticleRunnable != null){
+            mRetrieveArticleRunnable = null; // because we need to instantiate the new one as seen in below:
+        }
+        mRetrieveArticleRunnable = new RetrieveArticleRunnable(query, pageNumber);
+        final Future handler = AppExecutors.getInstance().networkIO().submit(mRetrieveArticleRunnable);
         AppExecutors.getInstance().networkIO().schedule(new Runnable() {
             @Override
             public void run() {
@@ -47,6 +57,63 @@ public class ArticleApiClient {
             }
         },NETWORK_TIMEOUT, TimeUnit.MICROSECONDS);
 
+    }
+
+    private class RetrieveArticleRunnable implements Runnable{
+
+        private String query;
+        private int pageNumber;
+        boolean cancelRequest;
+
+        public RetrieveArticleRunnable(String query, int pageNumber) {
+            this.query = query;
+            this.pageNumber = pageNumber;
+            cancelRequest = false;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Response response = getArticles(query,pageNumber).execute();
+                if(cancelRequest) {
+                    return;
+                }
+                if(response.code() == 200){
+                    List<Docs> list = new ArrayList<>(((ArticleSearchResponse)response.body()).getResponse().getDocs());
+                    if(pageNumber == 1){
+                        mDocs.postValue(list);
+                    }
+                    else{
+                        List<Docs> currentArticle = mDocs.getValue();
+                        currentArticle.addAll(list);
+                        mDocs.postValue(currentArticle);
+                    }
+                }
+                else{
+                    String error = response.errorBody().string();
+                    Log.d(TAG, "run: "+error);
+                    mDocs.postValue(null); // in future we detect null if find it so we figure
+                    // it out the is an error and let the user know
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                mDocs.postValue(null);
+            }
+
+
+        }
+        private Call<ArticleSearchResponse> getArticles(String query, int pageNumber){
+            return ServiceGenerator.getNytApi().searchArticle(
+                    Constants.API_KEY,
+                    query,
+                    String.valueOf(pageNumber)
+            );
+        }
+        private void cancelRequest() {
+            Log.d(TAG, "cancelRequest: canceling the request.");
+            cancelRequest = true;
+
+        }
     }
 
 }
